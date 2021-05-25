b15e4a2037906dbfbb184ecb2978ac0f1c91eab7
shun0918 <kkabosu386@gmail.com>
2021-05-26 08:39:40 +0900

    一時コミット

---
+++ app/components/templates/Index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>{{ cnt }}</h1>
+    <button @click="add()">+1</button>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+export default {
+  data() {
+    return {
+      cnt: 0
+    }
+  },
+  methods: {
+    add() {
+      this.cnt++;
+    },
+  }
+}
+</script>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,3 +1,19 @@
 <template>
-  <h1>Hello world!!</h1>
+  <div>
+    <NuxtLink to="/test">test</NuxtLink>  
+    <h1>Hello world!!</h1>
+    <Index />
+  </div>
+
 </template>
+
+<script>
+import Index from "~/components/templates/Index"
+export default {
+  components: {
+    Index
+  },
+  setup() {
+  },
+}
+</script>
